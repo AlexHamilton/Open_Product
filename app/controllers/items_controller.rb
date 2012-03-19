a84816ba241b15e41c97d6df26bde8bc6a25d19d
Morshed Alam <morshed201@gmail.com>
2012-03-20 01:21:37 +0600

    create/edit/show item

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,60 @@
+class ItemsController < ApplicationController
+
+  # GET /items/1
+  # GET /items/1.json
+  def show
+    @item = Item.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @item }
+    end
+  end
+
+  # GET /items/new
+  # GET /items/new.json
+  def new
+    @item = Item.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @item }
+    end
+  end
+
+  # GET /items/1/edit
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  # POST /items
+  # POST /items.json
+  def create
+    @item = Item.new(params[:item])
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to items_path, notice: 'Item created successfully.' }
+        format.json { render json: @item, status: :created, location: items_path }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /items/1
+  # PUT /items/1.json
+  def update
+    @item = Item.find(params[:id])
+
+    respond_to do |format|
+      if @item.update_attributes(params[:item])
+        format.html { redirect_to items_path, notice: 'Item updated successfully.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
